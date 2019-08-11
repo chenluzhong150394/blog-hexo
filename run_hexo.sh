a3697c44e6192010cf0093cf220a32a3574c461e
chenluzhong150394 <18438128833@163.com>
2019-08-11 15:24:57 +0800

    工作区更新时间：2019-08-11 15:24:40

--- run_hexo.sh
+++ run_hexo.sh
@@ -8,7 +8,7 @@ currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000))
 echo "时间戳: $currentTimeStamp"
 
 git add .
-git commit -m "$current"
+git commit -m "工作区更新时间：$current"
 git push -u origin master
 
 echo '更新成功'
