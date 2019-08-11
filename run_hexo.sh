a05a1b6a22f661ccbac27e5d50bd99ddf6ab30a2
chenluzhong150394 <18438128833@163.com>
2019-08-11 15:19:59 +0800

    $current

---
+++ run_hexo.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+##获取时间戳
+current=`date "+%Y-%m-%d %H:%M:%S"`  
+timeStamp=`date -d "$current" +%s`   
+#将current转换为时间戳，精确到毫秒  
+currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000))
+echo '时间戳: $currentTimeStamp'
+
+git add .
+git commit -m '$current'
+git push -u origin master
+
+echo '更新成功'
+
